8a1cb6e67f4ffb05ad789e78f9c026a2840c2f62
Andres Przybylko <andres.przybylko@gmail.com>
2022-02-03 14:29:17 -0300

    en canvas.js se hizo la funcion para dibujar linea por cada letra

--- canvas.js
+++ canvas.js
@@ -1,11 +1,22 @@
+var canvas = document.getElementById('canvas-ahorcado');
+var pincel = canvas.getContext('2d');
 
 function crearTableroJuego(){
-    var canvas = document.getElementById('canvas-ahorcado');
-    var pincel = canvas.getContext('2d');
 
     pincel.fillStyle = "#ffa74f";
     pincel.fillRect(0, 0, 1200, 800); // x, y, width, height
-   
 }
 
+function dibujarLineas(cantidadLineas){
+    x = 400;
+    for(i = 0; i < cantidadLineas; i++){
+	pincel.fillStyle = "black";
+        pincel.fillRect(x, 400, 30, 2);
+
+	x = x + 60
+
+    }
+
+
+}
 
