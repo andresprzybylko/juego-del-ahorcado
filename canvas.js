4c8bad8da4bb2a1b7fa963102d2ab381fb8513b4
Andres Przybylko <andres.przybylko@gmail.com>
2022-02-03 01:00:27 -0300

    Inicio del prog, se tomo como base lo dado por alura. cree funcion dibujar tablero

---
+++ canvas.js
@@ -0,0 +1,11 @@
+
+function crearTableroJuego(){
+    var canvas = document.getElementById('canvas-ahorcado');
+    var pincel = canvas.getContext('2d');
+
+    pincel.fillStyle = "#ffa74f";
+    pincel.fillRect(0, 0, 1200, 800); // x, y, width, height
+   
+}
+
+
