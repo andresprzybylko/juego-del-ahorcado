653ac24cf031cb363a76c411f3e08bf88e18c099
Andres Przybylko <andres.przybylko@gmail.com>
2022-02-03 14:30:32 -0300

    se agrego remover palabra de la lista para seguir jugando con otras palabras

--- juego.js
+++ juego.js
@@ -1,25 +1,43 @@
 
 var botonIniciarJuego = document.querySelector("#iniciar-juego");
+var listaPalabras = ["GATO", "PERRO", "JUEGUETE", "PELOTA", "TROMPO", "ESTUPEFACTO"];
 
 botonIniciarJuego.addEventListener("click",function(event){
     event.preventDefault();
 
     crearTableroJuego();
-    var palabraElegida = escogerPalabraSecreta();
+    var palabraElegida = escogerPalabraSecreta(listaPalabras);
+    console.log("Esta es la palabra elegida " + palabraElegida);
+    var letras = palabraElegida.split("");
+    console.log("Palabra dividida en letras: " + letras);
+    letras.forEach(function(letra){
+	
+	console.log("cada letra " + letra);
+
+    });
+
+    var cantidadLineas = palabraElegida.length;
+    dibujarLineas(cantidadLineas);
+    return palabraElegida;
     
 });
 
 
-function escogerPalabraSecreta(){
-    var listaPalabras = ["gato", "perro", "juguete", "pelota", "trompo"];
-    var seleccionAleatoria = listaPalabras[indiceAleatorio(listaPalabras)];
-    // si la palabra ya fue elegida previamente sacarla de la lista  
-    console.log(seleccionAleatoria);
+function escogerPalabraSecreta(listaPalabras){
 
+    var pos = indiceAleatorio(listaPalabras);
+    var seleccionAleatoria = listaPalabras[pos];
+    console.log("seleccion aleatoria de palabra " + seleccionAleatoria);
+    listaPalabras.splice(pos, 1);
+    console.log("Quedan " + listaPalabras.length + " palabras para jugar: " + listaPalabras);
+    //hacer funcion para escribir en pantalla las palabras eliminadas y tacharlas 
+    return seleccionAleatoria;
 }
 
 function indiceAleatorio(listaPalabras) {
     indice = Math.round(Math.random() * (listaPalabras.length -1));
-    console.log(indice)
     return indice;
 }
+
+
+
