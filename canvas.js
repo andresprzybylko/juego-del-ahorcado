a2b866f8424b020f5b5e616b2bc30f194ee680a1
Andres Przybylko <andres.przybylko@gmail.com>
2022-02-03 21:16:00 -0300

    agregue scroll al canvas al inicio del juego

--- canvas.js
+++ canvas.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('canvas-ahorcado');
 var pincel = canvas.getContext('2d');
 
 function crearTableroJuego(){
-
+    canvas.scrollIntoView();
     pincel.fillStyle = "#ffa74f";
     pincel.fillRect(0, 0, 1200, 800); // x, y, width, height
 }
@@ -12,11 +12,25 @@ function dibujarLineas(cantidadLineas){
     for(i = 0; i < cantidadLineas; i++){
 	pincel.fillStyle = "black";
         pincel.fillRect(x, 400, 30, 2);
-
 	x = x + 60
-
     }
-
-
 }
 
+
+
+
+/* para dibujar cada parte del ahorcado
+// First path
+ctx.beginPath();
+ctx.strokeStyle = 'blue';
+ctx.moveTo(20, 20);
+ctx.lineTo(200, 20);
+ctx.stroke();
+
+// Second path
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(20, 20);
+ctx.lineTo(120, 120);
+ctx.stroke();
+*/
